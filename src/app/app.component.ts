4d20c1f140c5c8585cc26175ed6364bc430b6e88
kiranreddy75 <saikirangagganeoalli@gmail.com>
2018-09-05 05:24:23 -0700

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [FormBuilder, FormGroup, Validators],
+
 })
+
 export class AppComponent {
-  title = 'app';
+  
+  rForm: FormGroup;
+  post: any;
+  description: string = '';
+  name: string = '';
+  constructor(private fb:FormBuilder){
+    this.rForm = fb.group ({
+      'name' : [null, Validators.required],
+      'description' : [null, Validators.compose([Validators.required, Validators.minLength(30), Validators.maxLength(500)])],
+    });
+
+  }
 }
